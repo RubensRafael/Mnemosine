f716d88e8a763a99923f86a854e75bdce8e9c032
RubensRafael <rubensrafael2@live.com>
2021-12-30 17:49:30 -0200

    css do pop up feito, falta atualizar side bar com redux

--- src/aware-components/folder.js
+++ src/aware-components/folder.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import '../style/side-bar.css';
 
 
 export default function Folder(props){
--- src/aware-components/new-folder.js
+++ src/aware-components/new-folder.js
@@ -1,14 +1,14 @@
 import {React, useState} from 'react';
 import request from '../request';
-import '../style/side-bar.css';
 import folderIcon from '../icons/new-folder.svg';
+import logo from '../icons/logo.png';
 import { CREATE_FOLDER } from '../querys';
 
 export default function NewFolder(){
 
 	let [newFolderPopUp , setNewFolderPopUp] = useState(false);
 	let [value , setValue] = useState('');
-	let [reqStatus , setReqStatus] = useState({warn:'',loading:false});
+	let [reqStatus , setReqStatus] = useState({error:false,warn:'',loading:false});
 
 	async function createNewFolder(e){
 		e.preventDefault()
@@ -18,11 +18,11 @@ export default function NewFolder(){
 				inputName : value
 			}
 		}).then((res)=>{
-			setReqStatus({warn:`The folder "${res.data.createFolder.name}" was created with sucess.`,loading:false})
+			setReqStatus({error:false,warn:`The folder "${res.data.createFolder.name}" was created with sucess.`,loading:false})
 		}).catch((err)=>{
-			setReqStatus({warn:err.networkError.result.errors[0].message,loading:false})
+			setReqStatus({error:true,warn:err.networkError.result.errors[0].message,loading:false})
 		})
-		setReqStatus({warn: '',loading:true})
+		setReqStatus({error:false,warn: '',loading:true})
 
 
 	}
@@ -34,17 +34,20 @@ export default function NewFolder(){
 				<p>New Folder</p>
 		</div> :
 		<div id='new-folder-popup'>
-			<div>
-				<p>{reqStatus.warn}</p>
-			</div>
-			<div>
-				<form onSubmit={createNewFolder}>
-					<input readOnly={reqStatus.loading} value={value} onChange={(e) => setValue(e.target.value)} type="text"></input>
-				</form>
-			</div>
-			<div>
-				<div onClick={() => setNewFolderPopUp(false)}>CANCELAR</div>
-				<div onClick={createNewFolder}>ENVIAR</div>
+			<div id='new-folder-box'>
+				<div>
+					<p id={reqStatus.error ? 'new-folder-error': 'new-folder-warn'} >{reqStatus.warn}</p>
+				</div>
+				<div id={reqStatus.loading ? 'new-folder-center-box-loading' : 'new-folder-center-box'}>
+					{reqStatus.loading ? <img id="new-folder-loading" src={logo} alt="Logo"></img> :
+					<form id="new-folder-form" onSubmit={createNewFolder}>
+						<input autoFocus={true} placeholder="Input the name of your folder." id="new-folder-input" value={value} onChange={(e) => setValue(e.target.value)} type="text"></input>
+					</form>}
+				</div>
+				<div>
+					<div id="new-folder-cancel" onClick={() => setNewFolderPopUp(false)}>Cancel</div>
+					<div id="new-folder-send" onClick={createNewFolder}>Send</div>
+				</div>
 			</div>
 		</div>}
 	</>	
