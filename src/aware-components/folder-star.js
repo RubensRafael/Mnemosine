639d69831e963563192f562620a72c2b5b2f527d
RubensRafael <70234898+RubensRafael@users.noreply.github.com>
2022-01-11 08:34:25 -0300

    set star part 1

--- src/aware-components/folder-star.js
+++ src/aware-components/folder-star.js
@@ -1,40 +1,30 @@
 import React from 'react';
-import SideBar from './side-bar';
-import DashboardHeader from './dashboard-header';
 import styled from 'styled-components';
-//import { useQuery } from '@apollo/client';
-//import { LOGIN_DEV_USER } from '../querys';
-//import client from '../request';
-
-export default function Dashboard(props){
+import { useMutation  } from '@apollo/client';
+import { CHANGE_FOLDER_TOMAIN  } from '../querys';
 
 
+export default function Star(props){
+    
+
+	let handleIsMainChanged = (data) =>{dispatch(update());dispatch(mainChanged(true  ))}
+	const [changeToMain , { data, loading, error }] = useMutation(CHANGE_FOLDER_TOMAIN  ,{
+		onCompleted : handleIsMainChanged,
+	});
+
+	const colorByDefault = actualFolder.isMain ? "yellow" : "transp"
+	const colorByLoad = loading ? "gray" : ''
 	
-	localStorage.setItem('token',"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxYzc3ZjFhMjczZGJjNGU3ZjY4NjliMiJ9.kKe3bM-OCfSRQgBVC8QcCvWxhE2d2l9aDxBFZTgKOOM")
 	
 	return (
-			<DashboardBox>
-				<SideBar></SideBar>
-				<DashboardContainer>
-					<DashboardHeader></DashboardHeader>
-				</DashboardContainer>
-			</DashboardBox>
+			<Star onClick={ changeToMain({ variables: { folderId:  actualfolder._id}} )} >{ colorByLoad || colorByDefault  }</Star>
 		)
 }
-const DashboardBox = styled.main`
+const Star  = styled.main`
 	
-	margin-top: 15vh;
-	height: 85vh;
-	display: flex;
-	background-color: gray;
 	
-	@media (max-width: 992px) {
+	
 
-	
-	display: flex;
-	flex-direction: column-reverse;
-	
-}
 `
 const DashboardContainer = styled.main`
 	display:  flex;
